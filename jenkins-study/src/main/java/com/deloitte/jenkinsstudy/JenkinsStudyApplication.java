5a991a658fb098beb57b445a445a0fca431e2f84
朱自立 <zhuzili@cqliving.com>
2022-04-30 21:35:00 +0800

    第一次提交代码

---
+++ jenkins-study/src/main/java/com/deloitte/jenkinsstudy/JenkinsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.deloitte.jenkinsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsStudyApplication.class, args);
+    }
+
+}
---
+++ jenkins-study/src/test/java/com/deloitte/jenkinsstudy/JenkinsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deloitte.jenkinsstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
